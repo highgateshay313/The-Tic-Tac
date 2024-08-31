6f51eaae50382544d38a19b3eea7d61f56f6ad02
Legacy <highgateshay@yahoo.com>
2024-08-31 17:31:59 -0500

    Place marker O position in the grid

--- shay.js
+++ shay.js
@@ -1,55 +1,22 @@
 
-const item1 = document.getElementById("item1");
-const item2 = document.getElementById("item2");
-const player1Btn = document.querySelector("#pName1");
-const addName = document.querySelector(".btn");
 
 
+function changeName() {
+    let input = document.getElementById("player1Name");
+    let input2 = document.getElementById("player2Name")
+    let out = document.querySelector("#pName1");
+    let out2 = document.querySelector("#pName2");
 
+    input.onkeyup = function () {
+        out.textContent = input.value;
+        out.style.background = "#000000"
+        return input.value;
+    }
 
-
-
-
-item1.addEventListener("click", () => {
-    const para = document.createElement("p");
-   para.textContent = "X";
-   item1.textContent = para.innerText;
-   item1.style.color = "white";
-   item1.style.fontSize = "2rem"
-})
-
-item2.addEventListener("click", () => {
-    const para2 = document.createElement("p");
-    para2.textContent = "O";
-    item2.textContent = para2.innerText;
-    item2.style.color = "white";
-    item2.style.fontSize = "2rem"
-})
-
-player1Btn.addEventListener("click", () => {
-    const nameForm = document.querySelector(".nameForm");
-    nameForm.style.zIndex = "2";
-})
-
-addName.addEventListener("click", (e) => {
-    let nameValue = document.getElementById("player1Name");
-    let test = nameValue.value;
-    let test2 = document.querySelector("#pName1");
-    test2.textContent = test;
-    addName.style.zIndex = "-2";
-})
-
-//Object for the gameboard
-const Gameboard = {
-
+    input2.onkeyup = function () {
+        out2.textContent = input2.value
+        out2.style.background = "#f25ce1"
+    }
 }
 
-//Object for the players
-const Players = {
-
-}
-
-//Object for game flow
-const GameFlow = {
-    
-}
+changeName()
